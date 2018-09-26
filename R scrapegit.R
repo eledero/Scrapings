a50c030b0d0d2d5b75d6264b98e74f3e399f05fd
eledero <eledero@hotmail.com>
2018-09-26 16:41:27 -0500

    Second commit

--- R scrapegit.R
+++ R scrapegit.R
@@ -4,28 +4,22 @@ library(RSelenium)
 #devtools::install_github("ropensci/RSelenium")
 #Initialize RSelenium
 
-print("Initializing Remote Drive...")
 remDr <- remoteDriver(remoteServerAddr = "localhost", port = 4445L, browserName = "firefox")
 remDr$open(silent = TRUE)
 
 url <- "https://gitcoin.co/explorer?network=mainnet&idx_status=open&order_by=-web3_created"
-
-print("Remote driver initialized. Browsing to the list of projects...")
 remDr$navigate(url)
 Sys.sleep(5)
 
-
-print("List of projects ready. Pressing end to get all of the projects. Let's wait a couple of seconds for the full list to load... ")
 body = remDr$findElement(using = 'css', value = "body")
 body$sendKeysToElement(list(key = "end"))
 #html body.interior.dashboard.g-font-muli div#dashboard-content.row.no-gutter div.col-12.col-lg-9.col-xl-10.body div#bounties.container-fluid div#dashboard-title div.row.mt-2.mb-2 div.col-12.offset-lg-1.col-lg-10.title-row div.col-12.col-md-4 span#filter.font-title
 
-Sys.sleep(15)
+Sys.sleep(10)
 elems = remDr$findElements(using = 'css', value = "a.row")
 links = unlist(sapply(1:length(elems), function(x) elems[[x]]$getElementAttribute("href")))
 links
 
-print("Full list retrieved. Starting to scrape all projects on the list...")
 getData <- function(x){
   
   remDr$navigate(links[x])
@@ -54,9 +48,7 @@ getData <- function(x){
   description <- (remDr$findElement(using = 'css', value = "#issue_description"))$getElementText()
   
   print(paste0("Project # ", x, " processed."))
-
-
- defList = c(title, 
+  return(list(title, 
               status, 
               opened, 
               openedTime, 
@@ -70,17 +62,8 @@ getData <- function(x){
               timeCommitment, 
               permission, 
               permission_type, 
-              description)
-
-defList = lapply(defList, function(x) ifelse(x == "", NA, x))
-
-print(defList) 
-
-
-
-  return(defList)  
+              description))  
 }
 
-a <- lapply(1:4, getData)
-#a <- lapply(1:length(links), getData)
+a <- lapply(1:length(links), getData)
 b <- data.frame(matrix(unlist(a), ncol = 15))
